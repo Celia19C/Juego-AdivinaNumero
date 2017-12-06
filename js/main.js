eb1097e0fd5891f82302ebf30931dfd5b52e4351
Celia19C <thatcyclone@gmail.com>
2017-12-06 17:35:12 +0100

    pistas o numero mayores/menores o letras

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ var score = document.querySelector('.tries');
 var button = document.querySelector('.boton_try');
 var puntos=1;
 var pista=document.querySelector ('.pista');
+var guardar=document.querySelector ('.name')
 
 
 //El número aleatorio NO con cada click, fuera
@@ -17,18 +18,31 @@ var aleatorio2 = Math.ceil(aleatorio);
 
 
 //El numero de intentos al hacer click
-	button.addEventListener('click', function(){
-		console.log(aleatorio2);
-		score.innerHTML=puntos++;
+button.addEventListener('click', function(){
+
 //Las pistas o acierto
 var numero= document.querySelector ('#box_number').value;
+var numero = parseInt(numero);
+//Cuando es un número del 0 al 100
+	if (numero<101 && numero>0){
+		console.log(aleatorio2);
+		score.innerHTML=puntos++;
 		if (numero<aleatorio2) {
 			pista.innerHTML= 'Te has quedado corta ¡Prueba otra vez!'
 		}
 		else if (numero>aleatorio2) {
 			pista.innerHTML='Te has pasado ¡Inténtalo de nuevo!'
 		}
-		else {
+		else if (numero==aleatorio2){
 			pista.innerHTML='¡¡Enhorabuena, has acertado!!'
-		};
+			// guardar.classList.remove ('.name')
+			// guardar.classList.add ('.open')
+		}}
+//Cuando o no es un número o es menor de cero o mayor de 100.
+	else if (numero>100 || numero<0) {
+		alert ('Solo se permiten números del 0 al 100.')
+	}
+	else {
+		alert ('Escribe un número con dígitos.')
+	}
 });
