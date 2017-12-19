51b18e6152a6f08936f4557735870e9d1949d3c3
Celia19C <thatcyclone@gmail.com>
2017-12-19 15:51:15 +0100

    correcciones evaluación

--- js/main.js
+++ js/main.js
@@ -11,10 +11,10 @@ var guardar=document.querySelector('.name');
 // Antes no era función, pero ahora sí porque luego, cuando acierten, la necesitaré para crear otro número nuevo.
 function aleatorio3 (max){
 
- 	var aleatorio = Math.random()*max;
- 	 		aleatorio = Math.ceil(aleatorio);
-			console.log(aleatorio);
-			return aleatorio;
+	var aleatorio = Math.random()*max;
+	aleatorio = Math.ceil(aleatorio);
+	console.log(aleatorio);
+	return aleatorio;
 
 }
 var aleatorio2 = aleatorio3(50);
@@ -22,10 +22,10 @@ var aleatorio2 = aleatorio3(50);
 
 //El click
 button.addEventListener('click', function(){
-//El input como valor
-var numero= document.querySelector('#box_number').value;
-	 	numero=parseInt(numero);
-//Cuando es un número del 0 al 100
+	//El input como valor
+	var numero= document.querySelector('#box_number').value;
+	numero=parseInt(numero);
+	//Cuando es un número del 0 al 100
 	if (numero<101 && numero>=0){
 		//El numero de intentos al hacer click
 		puntos=puntos+1;
@@ -41,11 +41,13 @@ var numero= document.querySelector('#box_number').value;
 			pista.innerHTML= '¡¡Enhorabuena, has acertado!!';
 			guardar.classList.remove('name');
 			guardar.classList.add('open');
-		}}
-//Cuando o no es un número o es menor de 0 o mayor de 100.
+		}
+	}
 	else if (numero>100 || numero<0) {
+		//Cuando o no es un número o es menor de 0 o mayor de 100.
 		alert ('Solo se permiten números del 0 al 100.');
 	}
+
 	else {
 		alert ('Escribe un número con dígitos.');
 	}
@@ -67,11 +69,11 @@ var nombres = {
 
 //Hacer click en el botón guardar
 boton_s.addEventListener('click', function(){
-//Número aleatorio nuevo.Llamando a la función
+	//Número aleatorio nuevo.Llamando a la función
 	// aleatorio3(50);
 	aleatorio2 = aleatorio3(100);
 	// console.log(aleatorio2);
-//Histórico con objetos y dentro 2 arrays (nombres e intentos)
+	//Histórico con objetos y dentro 2 arrays (nombres e intentos)
 
 	function historicoLista () {
 		var nombre= document.querySelector('#box_name').value;
@@ -84,19 +86,19 @@ boton_s.addEventListener('click', function(){
 		console.log(nombres);
 		var list_hi= nombres.intentos_s;
 		// console.log(list_hi);
-//Para que se guarden uno después de otro, se acumulen.
+		//Para que se guarden uno después de otro, se acumulen.
 		var add ='';
-			for (var i=0;i<list_h.length;i++){
-				add+= '<li>'+ list_h[i] +' - ' + list_hi[i] +' intentos </li>';
-			};
+		for (var i=0;i<list_h.length;i++){
+			add+= '<li>'+ list_h[i] +' - ' + list_hi[i] +' intentos </li>';
+		};
 		hstrc.innerHTML= add;
 	}
 	historicoLista();
-//Resetear
+	//Resetear
 	function reseteo (){
 		//Los inputs
 		document.querySelector('#box_number').value = "";
-  	document.querySelector ('#box_name').value = "";
+		document.querySelector ('#box_name').value = "";
 		//El marcador a 0 otra vez y las pistas
 		puntos=0;
 		score.innerHTML=puntos;
@@ -104,6 +106,6 @@ boton_s.addEventListener('click', function(){
 		//Volver a ocultar lo de guardar el nombre
 		guardar.classList.remove('open');
 		guardar.classList.add('name');
-		}
-		reseteo();
+	}
+	reseteo();
 });
